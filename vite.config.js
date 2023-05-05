1bfae95507e90b3306f4fa5b2668d6185b4c93c9
shinjith-dev <shinjithpallikkara@gmail.com>
2023-05-05 20:01:38 +0530

    for hosting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,8 @@ export default defineConfig({
     alias: [
       {
         find: './runtimeConfig',
-        replacement: './runtimeConfig.browser'
-      }
-    ]
-  }
-})
+        replacement: './runtimeConfig.browser',
+      },
+    ],
+  },
+});
