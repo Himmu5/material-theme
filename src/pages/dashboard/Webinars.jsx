ecf228c6aca66872b4b5efe8669e9d0b3c5f52cd
shinjith-dev <shinjithpallikkara@gmail.com>
2023-07-05 12:41:31 +0530

    completed design

--- src/pages/dashboard/Webinars.jsx
+++ src/pages/dashboard/Webinars.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import { Box, LinearProgress, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import WorkshopList from '../../components/workshops/WorkshopList';
+import AddWorkshop from '../../components/workshops/AddWorkshop';
+import AddWebinar from '../../components/webinars/AddWebinar';
+import WebinarList from '../../components/webinars/WebinarList';
 
 function Webinars() {
-  return <div>Webinars</div>;
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        height: '100vh',
+      }}
+    >
+      {loading && (
+        <LinearProgress
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+          }}
+          color="primary"
+        />
+      )}
+      <Box
+        sx={{
+          py: 3,
+          px: 5,
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          gap: 1,
+        }}
+      >
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography typography="h3" fontWeight={600}>
+            Webinars
+          </Typography>
+
+          <AddWebinar />
+        </Box>
+
+        <WebinarList />
+      </Box>
+    </Box>
+  );
 }
 
 export default Webinars;
